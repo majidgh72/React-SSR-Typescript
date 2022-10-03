cbcda64be7dd76594a5289d65d37795c42e76ff2
Majid Ghafoorzade <m_ghafoorzadeh@namava.local>
2022-10-04 03:18:16 +0330

    Init

---
+++ src/client.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { hydrateRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
+import App from "./App";
+
+hydrateRoot(
+  document.getElementById("root"),
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
+
+if (module.hot) {
+  module.hot.accept();
+}
